3b948186985ff398e08d81dbbeda12f4e93f2c00
kariminus <karim.meciel@gmail.com>
2016-12-12 14:46:06 +0100

    verification pour les demi-journées

---
+++ web/js/dayTime.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+    $("#reservation_date").change(function () {
+        var newDate = new Date(),
+            hours = newDate.getHours(),
+            today = newDate.toLocaleDateString(),
+            date = $("#reservation_date").val();
+        console.log(newDate);
+        console.log(hours);
+        if (date == today && hours >= 14) {
+
+        $("#reservation_dayTime").prop("checked", true);
+        $("#reservation_dayTime").prop("required", true);
+
+        }
+        else {
+            $("#reservation_dayTime").prop("checked", false);
+            $("#reservation_dayTime").prop("required", false);
+        }
+    });
+
+});
